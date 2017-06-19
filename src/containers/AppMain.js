090da310b5ed6ea25f9f77ffc713ec777e99468f
hacker-box <hackerhypenbox@gmail.com>
2017-06-18 21:47:57 -0700

    Add selector test support

--- src/containers/AppMain.js
+++ src/containers/AppMain.js
@@ -2,12 +2,14 @@ const React = require("react");
 const { BrowserRouter, Route } = require("react-router-dom");
 const StandardLayout = require("../common/containers/StandardLayout");
 const ActionTrigger = require("./test/ActionTrigger");
+const SelectorTrigger = require("./test/SelectorTrigger");
 
 const AppMain = () => (
   <BrowserRouter>
     <StandardLayout>
       <Route path="/" exact render={() => <h1>:-)</h1>} />
       <Route path="/test/action" component={ActionTrigger} />
+      <Route path="/test/selector" component={SelectorTrigger} />
     </StandardLayout>
   </BrowserRouter>
 );
---
+++ src/containers/test/SelectorTrigger.js
@@ -0,0 +1,10 @@
+const React = require("react");
+const { DevConsole } = require("../../common/components/devconsole");
+
+class SelectorTrigger extends React.Component {
+  render() {
+    return <DevConsole done={true} />;
+  }
+}
+
+module.exports = SelectorTrigger;
